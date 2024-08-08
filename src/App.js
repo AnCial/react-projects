9e5d2adbf5e66c3a1b901549bf9c4de3f217ad3a
AnCial <aguinaldo.marcial1@gmail.com>
2024-08-08 11:50:17 +0800

    115 start

--- src/App.js
+++ src/App.js
@@ -128,27 +128,6 @@ function Box({ children }) {
   );
 }
 
-// function Watchedbox() {
-
-//   const [isOpen2, setIsOpen2] = useState(true);
-
-//   return (
-//     <div className="box">
-//       <button
-//         className="btn-toggle"
-//         onClick={() => setIsOpen2((open) => !open)}
-//       >
-//         {isOpen2 ? "–" : "+"}
-//       </button>
-//       {isOpen2 && (
-//         <>
-
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-
 function MovieList({ movies }) {
   return (
     <ul className="list">
